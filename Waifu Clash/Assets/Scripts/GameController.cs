53eef066565e48c1553fb9c3421f78a44bc616e5
pompeiusSA <ponpeiusu.sa@gmail.com>
2026-06-06 23:20:46 -0300

    HUD
    Prototipo da hud feita com sucesso

--- Waifu Clash/Assets/Scripts/GameController.cs
+++ Waifu Clash/Assets/Scripts/GameController.cs
@@ -1,10 +1,25 @@
+using Unity.VisualScripting;
 using UnityEngine;
+
+public enum dispositivo
+{
+    mobile,
+    pc,
+}
 
 public class GameController : MonoBehaviour
 {
     [Header("HUD")]
 
-    public GameObject[] botoesCamera;
+    [SerializeField] dispositivo dispositivoAtual;
+
+    public Transform[] posBases; public Vector2 scaleBasesMobile, scaleBasesPC;
+
+    public RectTransform[] botoesSuperiores; public Vector2[] posBataoSuperiorMobile, posBataoSuperiorPC;
+
+    public RectTransform molduraWaifu; public Vector2 posMolduraMobile, posMolduraPC;
+
+    [Header("Cenario")]
 
     public Camera mainCamera;
 
@@ -13,13 +28,53 @@ public class GameController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        adaptandoHUD();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void adaptandoHUD()
+    {
+        //Dependendo do dispositivo,a escala do HUD e objetos mudam
+
+        if (dispositivoAtual == dispositivo.mobile)
+        {
+            for (int i = 0; i < posBases.Length; i++)
+            {
+                posBases[i].localScale = scaleBasesMobile;
+            }
+
+            //0 é moeda, 1 é upgrades e 2 é configurações
+
+            botoesSuperiores[0].anchoredPosition = posBataoSuperiorMobile[0];
+            botoesSuperiores[1].anchoredPosition = posBataoSuperiorMobile[1];
+            botoesSuperiores[2].anchoredPosition = posBataoSuperiorMobile[2];
+
+            //Ajustando o moldura
+
+            molduraWaifu.anchoredPosition = posMolduraMobile;
+        }
+        else
+        {
+            for (int i = 0; i < posBases.Length; i++)
+            {
+                posBases[i].localScale = scaleBasesPC;
+            }
+
+            //0 é moeda, 1 é upgrades e 2 é configurações
+
+            botoesSuperiores[0].anchoredPosition = posBataoSuperiorPC[0];
+            botoesSuperiores[1].anchoredPosition = posBataoSuperiorPC[1];
+            botoesSuperiores[2].anchoredPosition = posBataoSuperiorPC[2];
+
+            //Ajustando o moldura
+
+            molduraWaifu.anchoredPosition = posMolduraPC;
+        }
     }
 
     public void mudandoPosCamera(GameObject botao)
